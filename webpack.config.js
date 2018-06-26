0b10fff5b81c124896e358f838195a56d1a49f58
Michał Brusiło <mibru@interia.pl>
2018-06-26 13:21:27 +0200

    Add app.production.bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,39 @@ const path = require('path');
 
 //webpack.config.js
 module.exports = (env) => {
+    const environment = env || 'production';
     return {
-        mode: env || 'production',
-        entry: './src/app.js',
-    output: {
+        mode: environment,
+        entry: './src/index.js',
+        output: {
             path: path.resolve(__dirname, 'build'),
             filename: 'app.' + environment + '.bundle.js'
         },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-        test: /\.css$/,
-        use: [
-            { loader: 'style-loader'},
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: true
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader'},
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
                 }
-            }
-        ]
-    }
-        ]
-    }
-    optimization: {
-        minimize: false
+            ]
+        },
+
+        optimization: {
+            minimize: false
+        },
+        
     }
 };
